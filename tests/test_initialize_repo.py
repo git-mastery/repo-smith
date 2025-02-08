fe4f1131e27d006863a3c12aba3e9c962b62ed10
Woo Jia Hao <woojiahao1234@gmail.com>
2025-02-08 16:56:40 +0800

    Add unit tests for step

--- tests/test_initialize_repo.py
+++ tests/test_initialize_repo.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 from git import Repo
 
@@ -9,11 +7,6 @@ from src.repo_smith.initialize_repo import initialize_repo
 def test_initialize_repo_no_name() -> None:
     with pytest.raises(Exception):
         initialize_repo("tests/specs/no_name.yml")
-
-
-def test_initialize_repo_step_no_name() -> None:
-    with pytest.raises(Exception):
-        initialize_repo("tests/specs/step_no_name.yml")
 
 
 def test_initialize_repo_basic_spec() -> None:
@@ -67,68 +60,5 @@ def test_initialize_repo_post_hook() -> None:
 
     repo_initializer = initialize_repo("tests/specs/basic_spec.yml")
     repo_initializer.add_post_hook("initial-commit", initial_commit_post_hook)
-    with repo_initializer.initialize():
-        pass
-
-
-# TODO move these to separate files for broader unit testing?
-def test_initialize_repo_new_file() -> None:
-    def validate_filea_hook(r: Repo) -> None:
-        dir_list = os.listdir(r.working_dir)
-        assert "filea.txt" in dir_list
-        filepath = os.path.join(r.working_dir, "filea.txt")
-        expected_file_contents = ["Hello world!", "", "This is a file"]
-        with open(filepath, "r") as f:
-            lines = [line.strip() for line in f.readlines()]
-            assert len(lines) == len(expected_file_contents)
-            for actual, expected in zip(lines, expected_file_contents):
-                assert actual == expected
-
-    def validate_nested_file_hook(r: Repo) -> None:
-        dir_list = os.listdir(r.working_dir)
-        assert "nested" in dir_list
-        filepath = os.path.join(r.working_dir, "nested/a/b/c/filed.txt")
-        assert os.path.isfile(filepath)
-        expected_file_contents = ["This is a nested file"]
-        with open(filepath, "r") as f:
-            lines = [line.strip() for line in f.readlines()]
-            assert len(lines) == len(expected_file_contents)
-            for actual, expected in zip(lines, expected_file_contents):
-                assert actual == expected
-
-    repo_initializer = initialize_repo("tests/specs/new_file.yml")
-    repo_initializer.add_post_hook("filea", validate_filea_hook)
-    repo_initializer.add_post_hook("nested_file", validate_nested_file_hook)
-    with repo_initializer.initialize():
-        pass
-
-
-def test_initialize_repo_edit_file() -> None:
-    def validate_filea_hook(r: Repo) -> None:
-        dir_list = os.listdir(r.working_dir)
-        assert "filea.txt" in dir_list
-        filepath = os.path.join(r.working_dir, "filea.txt")
-        expected_file_contents = ["Hello world!", "", "This is a file"]
-        with open(filepath, "r") as f:
-            lines = [line.strip() for line in f.readlines()]
-            assert len(lines) == len(expected_file_contents)
-            for actual, expected in zip(lines, expected_file_contents):
-                assert actual == expected
-
-    def validate_nested_file_hook(r: Repo) -> None:
-        dir_list = os.listdir(r.working_dir)
-        assert "nested" in dir_list
-        filepath = os.path.join(r.working_dir, "nested/a/b/c/filed.txt")
-        assert os.path.isfile(filepath)
-        expected_file_contents = ["This is a nested file"]
-        with open(filepath, "r") as f:
-            lines = [line.strip() for line in f.readlines()]
-            assert len(lines) == len(expected_file_contents)
-            for actual, expected in zip(lines, expected_file_contents):
-                assert actual == expected
-
-    repo_initializer = initialize_repo("tests/specs/new_file.yml")
-    repo_initializer.add_post_hook("filea", validate_filea_hook)
-    repo_initializer.add_post_hook("nested_file", validate_nested_file_hook)
     with repo_initializer.initialize():
         pass
