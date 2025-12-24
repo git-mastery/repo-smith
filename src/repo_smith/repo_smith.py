00afbd87a3b399742b8748b5cb139d0d547361e3
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-24 13:25:23 +0800

    Add RepoSmith class

--- src/repo_smith/repo_smith.py
+++ src/repo_smith/repo_smith.py
@@ -1,2 +1,9 @@
+from repo_smith.helpers.files_helper import FilesHelper
+from repo_smith.helpers.git_helper.git_helper import GitHelper
+
+
 class RepoSmith:
-    pass
+    def __init__(self, verbose: bool) -> None:
+        self.verbose = verbose
+        self.files = FilesHelper(verbose)
+        self.git = GitHelper(verbose)
