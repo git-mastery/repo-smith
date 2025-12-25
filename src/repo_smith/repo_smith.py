e86a90b265ba1a5db52edd46635a556cda3941ba
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-25 15:35:24 +0800

    Adding generic Helper adder
    This is so we don't need to embed Git-Mastery specific logic into this
    class but we still want to ensure that all Helpers are initialized in a
    consistent manner

--- src/repo_smith/repo_smith.py
+++ src/repo_smith/repo_smith.py
@@ -1,12 +1,14 @@
 import tempfile
 from contextlib import contextmanager
-from typing import Iterator, TypedDict, Unpack
+from typing import Dict, Iterator, Self, Type, TypedDict, TypeVar, Unpack
 
 from git.repo import Repo
 
 from repo_smith.helpers.files_helper import FilesHelper
 from repo_smith.helpers.git_helper.git_helper import GitHelper
-from repo_smith.helpers.gitmastery_helper import GitMasteryHelper
+from repo_smith.helpers.helper import Helper
+
+T = TypeVar("T", bound="Helper")
 
 
 class RepoSmith:
@@ -15,7 +17,16 @@ class RepoSmith:
         self.repo = repo
         self.files = FilesHelper(repo, verbose)
         self.git = GitHelper(repo, verbose)
-        self.git_mastery = GitMasteryHelper(repo, verbose)
+        self.__additional_helpers: Dict[Type[Helper], Helper] = {}
+
+    def add_helper(self, cls: Type[T]) -> Self:
+        self.__additional_helpers[cls] = cls(self.repo, self.verbose)
+        return self
+
+    def helper(self, cls: Type[T]) -> T:
+        if cls not in self.__additional_helpers:
+            raise ValueError(f"Additional helper of {cls.__name__} not found.")
+        return self.__additional_helpers[cls]
 
 
 class CreateRepoOptions(TypedDict, total=False):
