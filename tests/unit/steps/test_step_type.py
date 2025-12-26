bacf09bcd70291928c40d10f27891aefbf85e10c
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-26 16:45:39 +0800

    Remove old repo-smith supports
    Remove old repo-smith integration tests
    
    Remove old repo-smith unit tests
    
    Clean up README
    
    Remove specification
    
    No longer require it since we're not maintaining the specification.
    
    Documentation should come in the form of docstrings now.
    
    Fix new repo-smith unit tests

--- tests/unit/steps/test_step_type.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from repo_smith.steps.step_type import StepType
-
-
-def test_step_type_uncovered_type():
-    with pytest.raises(ValueError):
-        StepType.from_value("this should not be implemented")
